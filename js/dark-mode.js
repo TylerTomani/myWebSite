d248c8c8295790c3fec21c30c1ea1ffc86fd5343
Tyler Tomani <ttomani@gmail.com>
2025-06-14 12:33:45 -0400

    main

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -5,19 +5,19 @@ addEventListener('keydown', e => {
     let isShiftPressed = e.shiftKey
 
     if (isShiftPressed && letter == 'd') {
-        body.classList.toggle('darkMode')
+        body.classList.toggle('dark-mode')
     }
 })
 
 darkmodeBtn.addEventListener('click', (e) => { 
     e.preventDefault()
-    body.classList.toggle('darkMode') 
+    body.classList.toggle('dark-mode') 
 })
 darkmodeBtn.addEventListener('keydown', (e) => {
     let letter = e.key.toLowerCase()
     if (letter == 'enter') {
         e.preventDefault()
-        body.classList.toggle('darkMode')
+        body.classList.toggle('dark-mode')
     }
 
 })
